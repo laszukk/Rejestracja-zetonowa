9ca714f3679d30ce7e31a701ec13e5246d2500d8
laszukk <iriski.2k18@gmail.com>
2022-01-22 14:24:37 +0100

    projekt wersja 1.1

--- zetonApp/tests.py
+++ zetonApp/tests.py
@@ -1,29 +1,45 @@
 import django
 from django.test import TestCase
-from zetonApp.models import Kursant
+from zetonApp.models import Instruktor, Kursant
+from rest_framework import status
+from django.contrib.auth.models import User
 
-def test__add_kursant(authorized_client):
-    data_to_save = {
-        'imie': 'Pan',
-        'nazwisko': 'Kursant',
-        'pesel': '00220710895',
-        'email': 'pankurs@gmail.com',
-    }
+class ModelKursantTest(TestCase):
+    def test_create_kursant(self):
+        User.objects.create_superuser(username='superuser',password='user12345', email='admin@example.com')
+        self.client.login(username='superuser',password='user12345')
+        response = self.client.post("/api/kursant/", {
+        "imie": "Jesse",
+        "nazwisko": "Pinkman",
+        "pesel": "00220710895", 
+        "email": "diesel@email.com", })
 
-    url = 'http://127.0.0.1:8000/api/kursant'
+        kursant = Kursant.objects.first()
 
-    response = authorized_client.post(
-        url,
-        data_to_save,
-        content_type='application/json',
-    )
-    assert response.status_code == 200
-    assert response.json() == data_to_save
-    all_kursanci = Kursant.objects.all()
-    assert all_kursanci.count() == 1
+        self.assertEqual(kursant.imie, "Jesse")
+        self.assertEqual(kursant.nazwisko, "Pinkman")
+        self.assertEqual(kursant.pesel, "00220710895")
+        self.assertEqual(kursant.email, "diesel@email.com")
+        self.assertEqual(response.status_code, 201)
 
-    saved_kursant = all_kursanci.first()
-    assert saved_kursant.imie == 'Pan'
-    assert saved_kursant.nazwisko == 'Kursant'
-    assert saved_kursant.pesel == '00220710895'
-    assert saved_kursant.email == 'pankurs@gmail.com'
+    def test_create_instruktor(self):
+            User.objects.create_superuser(username='superuser',password='user12345', email='admin@example.com')
+            self.client.login(username='superuser',password='user12345')
+            response = self.client.post("/api/instruktor/", {
+            "imie": "Walter",
+            "nazwisko": "White",
+            "przedmiot": "muzyka",
+            "pesel": "99220710895", 
+            "email": "heisenberg@email.com", 
+            "tytul_naukowy": "Magister",})
+
+            instruktor = Instruktor.objects.first()
+
+            self.assertEqual(instruktor.imie, "Walter")
+            self.assertEqual(instruktor.nazwisko, "White")
+            self.assertEqual(instruktor.przedmiot, "muzyka")
+            self.assertEqual(instruktor.pesel, "99220710895")
+            self.assertEqual(instruktor.email, "heisenberg@email.com")
+            self.assertEqual(instruktor.tytul_naukowy, "Magister")
+            self.assertEqual(response.status_code, 201)
+
